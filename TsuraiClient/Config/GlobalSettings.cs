2f54fe1fb36b1c8c38e1397ad708f3b5ee6c2774
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-10-29 16:21:46 +0900

    Ignored config file

--- TsuraiClient/Config/GlobalSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace TsuraiClient
-{
-	public static class GlobalSettings
-	{
-		public static string EmotionAPI_KEY = "<INPUT YOUR KEY>";
-		public static string EmotionAPI_BASE_URL = "https://api.projectoxford.ai/emotion/v1.0/";
-	}
-}
